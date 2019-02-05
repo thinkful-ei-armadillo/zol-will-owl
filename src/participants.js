dfec8c11e7d7dbd78e60e1ddd43c25ad12a94e1e
William Y Wong <wwong1987@gmail.com>
2019-02-05 12:18:31 -0800

    added component files

---
+++ src/participants.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function User(props) {
+
+    return (
+        <div className='user'>
+            
+        </div>
+    )
+}
+
+
+function Participants(props) {
+
+    return (
+    // id
+    // name
+    // avatar
+    // session: true or false
+    )
+
+}
+
+
+export default Participants;
